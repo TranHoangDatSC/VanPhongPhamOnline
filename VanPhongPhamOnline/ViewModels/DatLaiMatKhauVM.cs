693b312c32bc2f9eb83cf58a0b4bac43e2119837
anhhanh1234 <anhhanh2452005@gmail.com>
2025-08-05 10:39:33 +0700

    #Đã sửa quên mật khẩu khách hàng

---
+++ VanPhongPhamOnline/ViewModels/DatLaiMatKhauVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VanPhongPhamOnline.ViewModels
+{
+    public class DatLaiMatKhauVM
+    {
+        public string Email { get; set; } = null!;
+
+        [Required, MinLength(6)]
+        public string MatKhauMoi { get; set; } = null!;
+
+        [Required, Compare("MatKhauMoi", ErrorMessage = "Mật khẩu nhập lại không khớp.")]
+        public string NhapLaiMatKhau { get; set; } = null!;
+    }
+}
---
+++ VanPhongPhamOnline/ViewModels/XacNhanMaVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VanPhongPhamOnline.ViewModels
+{
+    public class XacNhanMaVM
+    {
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng nhập mã xác nhận.")]
+        public string MaXacNhan { get; set; } = null!;
+    }
+}
